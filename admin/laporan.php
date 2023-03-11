8866bec73bc90f29a4e5292f4a745b101ebe0ec5
nasrulginanjar08 <nasrulginanjar124@gmail.com>
2023-03-11 11:45:18 +0700

    search

--- admin/laporan.php
+++ admin/laporan.php
@@ -1,7 +1,24 @@
+<?php 
+ include'../koneksi.php';
+ ?>
 
 <h5>Laporan Pembayaran SPP</h5>
 <a href="cetak-laporan.php" class="btn btn-primary"> Cetak Laporan </a></br>
 <hr>
+
+<form action="laporan.php" method="get">
+    <label>Cari :</label>
+    <input type="text" name="cari">
+    <input type="submit" value="Cari">
+</form>
+ 
+<?php 
+if(isset($_GET['cari'])){
+    $cari = $_GET['cari'];
+    echo "<b>Hasil pencarian : ".$cari."</b>";
+}
+?>
+
 <table class="table table-striped table-bordered">
     <tr class="fw-bold">
         <td>No</td>
@@ -15,13 +32,20 @@
         <td>Petugas</td>
         
     </tr>
+
     <?php
-        include'../koneksi.php';
+    if(isset($_GET['cari'])){
+        $cari = $_GET['cari'];
+        $data = mysqli_query($koneksi, "SELECT*FROM pembayaran WHERE nisn LIKE '%".$cari."%'");             
+    }else{
+        $data = mysqli_query($koneksi, "SELECT*FROM pembayaran");       
+    }
         $no = 1;
         $sql = "SELECT*FROM pembayaran,siswa,kelas,spp,petugas WHERE pembayaran.nisn=siswa.nisn AND siswa.id_kelas=kelas.id_kelas AND pembayaran.id_spp=spp.id_spp AND pembayaran.id_petugas=petugas.id_petugas ORDER BY tgl_bayar DESC";
         $query = mysqli_query($koneksi, $sql);
         foreach ($query as $data ){            
             ?>
+
             <tr>
                 <td><?php echo $no++; ?></td>
                 <td><?php echo $data['nisn'] ?></td>
@@ -36,6 +60,4 @@
             </tr>
             
       <?php } ?>
-
-   
 </table>
